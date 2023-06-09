9a6f126483758418a6c7559b84681a91e76f3349
Néstor David Leiva Mora <nestor.leiva.mora@est.una.ac.cr>
2023-06-09 01:07:06 -0600

    Proyecto v2 - Clone esta!
    Cambios v2:
    - Nombres carpetas proyecto
    - FireBase en lugar de SQL
    - Nivel 6 division moderado
    - Nivel 7 division avanzada
    - Nivel 8 mixto pero sin divisiones
    - Recupera el mejor resultado de la base + el nombre

---
+++ app/src/main/java/com/android/proyecto_frutas/SplashScreen.java
@@ -0,0 +1,41 @@
+package com.android.proyecto_frutas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash_screen);
+
+        //Agregar Animaciones
+        Animation animacion1 = AnimationUtils.loadAnimation(this, R.anim.desplazamiento_arriba);
+        Animation animacion2 = AnimationUtils.loadAnimation(this, R.anim.desplazamiento_abajo);
+
+        TextView tv_Splash = findViewById(R.id.tv_Splash);
+        ImageView logoImageView = findViewById(R.id.LogoImageView);
+
+        tv_Splash.setAnimation(animacion2);
+        logoImageView.setAnimation(animacion1);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(intent);
+
+            }
+        }, 4000);
+    }
+}
